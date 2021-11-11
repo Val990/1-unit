299b34c8da9cf8bb4a490bd4f897cd02347dd1e2
Valeriya <tmcxezsim5nn@mail.ru>
2021-11-11 14:34:01 +0300

    testing

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,65 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+
+    public CashbackHackService service = new CashbackHackService();
+
+    @Test
+    public void calculateCashbackFrom900() {
+
+        int amount = 900;
+        int expected = 100;
+
+        int actual = service.remain(amount);
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void calculateCashbackFrom700() {
+
+        int amount = 700;
+        int expected = 300;
+
+        int actual = service.remain(amount);
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void calculateCashbackFrom100() {
+
+        int amount = 100;
+        int expected = 900;
+
+        int actual = service.remain(amount);
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void calculateCashbackFrom2500() {
+
+        int amount = 2500;
+        int expected = 500;
+
+        int actual = service.remain(amount);
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void calculateCashbackFrom1000() {
+
+        int amount = 1000;
+        int expected = 0;
+
+        int actual = service.remain(amount);
+
+        assertEquals(actual, expected, "If amount is 1000, the application should not tell to buy for 1000 more");
+    }
+}
